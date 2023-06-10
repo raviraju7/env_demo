64d003280f0d4198aa4f5f3a773654146c097a92
raviraju7 <84903359+raviraju7@users.noreply.github.com>
2023-06-11 10:22:36 +1200

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,8 +1,9 @@
 import os
 
 # Access GitHub repository secrets
-env = os.environ.get('ENV')
+env = os.environ.get('env.env')
+env_n = os.environ.get('ENV.ENV')
 env_oth = os.environ.get('GITHUB_ENV')
 
 print('hello')
-print(f'this is {env} branch {env_oth}')
+print(f'this is {env} branch {env_n}')

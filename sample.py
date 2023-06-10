9a4e86091217522b0a0b6ef3122fd57943619874
raviraju7 <84903359+raviraju7@users.noreply.github.com>
2023-06-11 11:08:07 +1200

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ import sys
 env = os.environ.get('env')
 
 #incoorect way to get the parameter - env_n wont get the value
-#env_n = os.environ.get('ENV.ENV')
+env_n = os.environ.get('ENV.ENV')
 
 #ways to get the github env variables
 #env_oth = os.environ.get('GITHUB_ENV')

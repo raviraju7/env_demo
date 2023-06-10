931d0309c412defefad5dfe5ed871093e09d9a24
raviraju7 <84903359+raviraju7@users.noreply.github.com>
2023-06-11 11:06:09 +1200

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,12 +2,18 @@ import os
 import sys
 
 # Access GitHub repository secrets
+# correct way to receive the parameter
 env = os.environ.get('env')
-env_n = os.environ.get('ENV.ENV')
-env_oth = os.environ.get('GITHUB_ENV')
 
-e = sys.argv[1]
-print(e)
+#incoorect way to get the parameter - env_n wont get the value
+#env_n = os.environ.get('ENV.ENV')
+
+#ways to get the github env variables
+#env_oth = os.environ.get('GITHUB_ENV')
+
+#below method of receiving the parameter works
+#e = sys.argv[1]
+#print(e)
 
 print('hello')
 print(f'this is {env} branch {env_n}')
